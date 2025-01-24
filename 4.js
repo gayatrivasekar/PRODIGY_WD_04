57416ca723cf6c0abc70f1a35e9b76d03e503d3b
gayatrivasekar <vasekargayatri3@gmail.com>
2025-01-24 23:57:20 +0530

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,5 @@
+// Optional JavaScript code for interactive features (e.g., form validation)
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Your message has been sent!');
+});
